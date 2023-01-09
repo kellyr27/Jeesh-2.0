0416f6e79ba3ae878542943208d9cd7d49d2a900
Kelly <83458976+kellyr27@users.noreply.github.com>
2023-01-10 09:14:35 +1100

    Created Soldier and Army Class
    Defining functions for Soldier and Army Class

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
 /**
+ * COORDINATE
+ *  - Defined as an ARRAY of 3 dimensional co-ordinates (x, y, z)
+ * 
+ * DIRECTION
+ *  - Defined as an INTEGER direction the object is facing
+ *  - Direction key
+ *      0 - +x
+ *      1 - -x
+ *      2 - +y
+ *      3 - -y
+ *      4 - +z
+ *      5 - -z
+ * 
  * POSITION
- * - Defined as a 4 dimensional co-ordinate (x, y, z)
- *      - (x, y, z) being the objects location in 3D space
-//  *      - (d) the direction the object is facing
-//  * 
-//  * DIRECTION KEY
-//  * - The direction the object is facing
-//  * 0 - +x (default if direction does not matter, for example STARS are directionless)
-//  * 1 - -x
-//  * 2 - +y
-//  * 3 - -y
-//  * 4 - +z
-//  * 5 - -z
+ *  - Defined as an ARRAY of [COORDINATE, DIRECTION]
  */
 
 const ARENA_SIZE = 11
@@ -34,31 +36,99 @@ function generateRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
+class Soldier {
+    constructor (startingPosition) {
+        this.positions = {
+            0: startingPosition
+        }
+        this.deathIndex = -1
+    }
+
+    /**
+     * Checks whether the Soldier is alive at an given move.
+     */
+    isAlive (moveNum) {
+        if (this.deathIndex === -1) {
+            return true
+        }
+        else if (this.deathIndex > moveNum) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    /**
+     * Gets the Soldiers position at a given move.
+     * Does not check whether the Soldiers status (alive/dead) at a the given position
+     */
+    getPosition (moveNum) {
+        const foundPositionIndex = Object.keys(this.positions).reverse().find(el => parseInt(el) <= gameIndex)
+        return this.positions[foundPositionIndex]
+    }
+
+    /**
+     * Sets the Soliders position at an given move.
+     */
+    setPosition (moveNum, position) {
+        this.positions[moveNum] = position
+    }
+
+}
+
+class Army {
+    constructor(startingPositions) {
+        this.soldiers = this.#generateSoldiers(startingPositions)
+    }
+
+    /**
+     * Generates a list of Soldiers at each of the starting positions
+     */
+    #generateSoldiers(startingPositions) {
+        const soldiers = []
+
+        for (const position of startingPositions) {
+            soldiers.push(new Soldier(position))
+        }
+
+        return soldiers
+    }
+
+    /**
+     * Gets the number of Soldiers alive at a given move.
+     */
+    getAliveCount(moveNum) {
+        return this.soldiers.filter(soldier => soldier.isAlive(moveNum)).length
+    }
+}
+
 class GameState {
     constructor () {
-        this.starPositions = this.#generateStarPositions()
+        this.starCoordinates = this.#generateStarCoordinates()
+        this.currentMoveNum = 1
     }
 
     /**
      * Generates a list of random star positions for a random number of stars (up to MAX_STARS)
      * Stars cannot be touching the edge of the ARENA
      */
-    #generateStarPositions () {
+    #generateStarCoordinates () {
         const numStars = generateRandomInt(MAX_STARS)
-        const starPositions = []
+        const starCoordinates = []
 
         for (let i = 0; i < numStars; i++) {
-            starPositions.push([
+            starCoordinates.push([
                 generateRandomInt(9)+1,
                 generateRandomInt(9)+1,
                 generateRandomInt(9)+1
             ])
         }
 
-        return starPositions
+        return starCoordinates
     }
 
 }
 
 const testGameState = new GameState()
-console.log(testGameState.starPositions)
+console.log(testGameState.starCoordinates)
