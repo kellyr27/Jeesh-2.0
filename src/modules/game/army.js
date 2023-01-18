e079484bf7ece83c1e6cab510adfe2c0f36b884c
Kelly <83458976+kellyr27@users.noreply.github.com>
2023-01-19 07:52:59 +1100

    Refactoring - army.js
    With comments

---
+++ src/modules/game/army.js
@@ -0,0 +1,64 @@
+/**
+ * ARMY
+ * An Army is a Players set of Soldiers (game pieces).
+ * If every Soldier in an Army is dead, then that Player has either lost (if the other Army has remaining Soldiers) or a draw.
+ */
+
+class Army {
+
+    constructor(startingPositions) {
+        if (startingPositions !== undefined) {
+            this.soldiers = this.#createSoldiers(startingPositions)
+        }
+    }
+
+    /**
+     * Creates a clone of an Army
+     */
+    clone(existingArmy) {
+        this.soldiers = []
+
+        for (const soldier of existingArmy.soldiers) {
+            const newSoldier = new Soldier()
+            newSoldier.clone(soldier)
+            this.soldiers.push(newSoldier)
+        }
+
+    }
+
+    /**
+     * Generates a list of Soldiers at each of the starting positions
+     */
+    #createSoldiers(startingPositions) {
+        const soldiers = []
+
+        for (const position of startingPositions) {
+            soldiers.push(new Soldier(position))
+        }
+
+        return soldiers
+    }
+
+    /**
+     * Gets the number of Soldiers alive at a given move.
+     */
+    getAliveCount(moveNum) {
+        return this.soldiers.filter(soldier => soldier.isAlive(moveNum)).length
+    }
+
+    /**
+     * Gets an array of Soldier coordinates at an given move.
+     */
+    getCoordinates(moveNum) {
+        const positions = []
+
+        for (const soldier of this.soldiers) {
+            if (soldier.isAlive(moveNum)) {
+                positions.push(soldier.getPosition(moveNum)[0])
+            }
+        }
+
+        return positions
+    }
+
+}
