65ee743f89e117e608459865d081b3f8974a7457
Kelly <83458976+kellyr27@users.noreply.github.com>
2023-02-13 17:27:21 +1100

    Implemented test Bezier curve

--- src/modules/display/line.js
+++ src/modules/display/line.js
@@ -7,14 +7,29 @@ export default class LineDisplay {
 
     constructor(scene) {
         this.scene = scene
+        console.log(bezier)
         this.test()
     }
 
     test() {
         const points = [];
-        for (let j = 0.1; j < 10; j += 0.1) {
-            points.push(new THREE.Vector3(j, j, j));
+        // for (let j = 0.1; j < 10; j += 0.1) {
+        //     points.push(new THREE.Vector3(j, j, j));
+        // }
+
+        
+        var points1 = [
+            [-1.0, 0.0, 0.0],
+            [-0.5, 0.5, 0.0],
+            [0.5, -0.5, 0.0],
+            [1.0, 0.0, 0.0]
+        ];
+
+        for (var t = 0; t < 1; t += 0.01) {
+            var point = bezier(t, points1);
+            points.push(new THREE.Vector3(point[0], point[1], point[2]))
         }
+
         const geometry = new THREE.BufferGeometry().setFromPoints(points);
         const line = new MeshLine();
         line.setGeometry(geometry);
